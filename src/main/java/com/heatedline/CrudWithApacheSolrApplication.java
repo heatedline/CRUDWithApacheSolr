1e928975fbc274a266c973a9ee7c30f842fd23a0
supratim.c@finlabsindia.org <supratim.c@finlabsindia.com>
2020-03-07 18:41:58 +0530

    first commit

---
+++ src/main/java/com/heatedline/CrudWithApacheSolrApplication.java
@@ -0,0 +1,13 @@
+package com.heatedline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithApacheSolrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWithApacheSolrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heatedline/CrudWithApacheSolrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heatedline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWithApacheSolrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
